8cdd31f141ee9629d0ecbe76b3846b3b8e822877
Mann <mann61099@gmail.com>
2021-07-10 19:56:27 +0530

    added initial web scraping script

--- Python Scripts/KeywordSearch.py
+++ Python Scripts/KeywordSearch.py
@@ -5,65 +5,54 @@ returns True if any of the keywords is found in the text, False otherwise
 import re
 
 
-def check(text, keywords_list):
+def check(text):
+    keywords_list = [
+        "Software",
+        "Application",
+        "Product",
+        "Bespoke",
+        "Customize application",
+        "Customize product",
+        ".net application",
+        "bespoke dot net",
+        "custome dot net",
+        "enterprise dotnet application",
+        "Angular",
+        "React",
+        "Native",
+        "Full Stack",
+        "Mobile application",
+        "iOS",
+        "android",
+        "app ",
+        "enterprise mobile application",
+        "enterpise mobility solution",
+        "Magento",
+        "Magento solution provider",
+        "Ecommerce",
+        "Magento multistore",
+        "open sourse",
+        "php application",
+        "drupal",
+        "custom cms",
+        "joomla",
+        "website",
+        "web ",
+        "SAP Support",
+        "SAP B1 Support",
+        "SAP Business One Support",
+        "SAP B1 Add On",
+        "SAP Business One Add On",
+        "SAP B1 Implementation",
+        "SAP Business One Implementation",
+        "RPA Implementation",
+        "RPA Consultant",
+        "Digitial Transformation Experts",
+        "Digital Transformation Consultants",
+    ]
     keywords_list = list(map(lambda x: x.lower(), keywords_list))
     text = text.lower()
     words_re = re.compile("|".join(keywords_list))
     if words_re.search(text):
-        print(str(words_re.search(text)))
         return True
     return False
-
-
-text = "I'm looking for candidates who are 1-2 years out of school and have an interest in working in a mission-driven company within the healthcare sector of Boston. Candidates should have an administrative and/or customer service background and experience using a CRM system is a plus! Email me for consideration: cryan@monumentstaffing.net Location: Remote! Non-MA residents are welcome to apply. #hiring #customerservice #administrative #healthcare #remotejob"
-keywords_list = [
-    "Software",
-    "Application",
-    "Product",
-    "Bespoke",
-    "Customize application",
-    "Customize product",
-    ".net application",
-    "bespoke dot net",
-    "custome dot net",
-    "enterprise dotnet application",
-    "Angular",
-    "React",
-    "Native",
-    "Full Stack",
-    "Mobile application",
-    "iOS",
-    "android",
-    "app ",
-    "enterprise mobile application",
-    "enterpise mobility solution",
-    "Magento",
-    "Magento solution provider",
-    "Ecommerce",
-    "Magento multistore",
-    "open sourse",
-    "php application",
-    "drupal",
-    "custom cms",
-    "joomla",
-    "website",
-    "web ",
-    "SAP Support",
-    "SAP B1 Support",
-    "SAP Business One Support",
-    "SAP B1 Add On",
-    "SAP Business One Add On",
-    "SAP B1 Implementation",
-    "SAP Business One Implementation",
-    "RPA Implementation",
-    "RPA Consultant",
-    "Digitial Transformation Experts",
-    "Digital Transformation Consultants",
-]
-
-if check(text, keywords_list):
-    # Found
-    print("Found")
-else:
-    # Not found
-    print("Not found")
---
+++ Python Scripts/WebScrape.py
@@ -0,0 +1,70 @@
+import mechanicalsoup
+from mechanicalsoup import form
+import re
+import pandas as pd
+from KeywordSearch import check
+
+browser = mechanicalsoup.Browser()
+
+# login
+print("Logging in...")
+url = "https://www.linkedin.com"
+page = browser.get(url)
+html = page.soup
+form = html.select("form")[0]
+form.select("#session_key")[0]["value"] = "f20190790@pilani.bits-pilani.ac.in"
+form.select("#session_password")[0]["value"] = "rrmk1662"
+home_page = browser.submit(form, page.url)
+print("Logged in")
+
+# linkedin search query
+search_for = "looking for remote"
+
+# number of pages to look for posts
+result_pages = 3
+
+search = re.sub("\s", "%20", search_for)
+search_url_base = f'https://www.linkedin.com/search/results/content/?datePosted="past-24h"&keywords={search}&origin=FACETED_SEARCH'
+
+result_links = []
+print("Searching for posts...")
+for i in range(1, result_pages + 1):
+    print("Page:", i)
+    if i == 1:
+        search_page_url = search_url_base
+    else:
+        search_page_url = search_url_base + f"&page={i}"
+    # print(search_page_url)
+    search_page = browser.get(search_page_url)
+    text = search_page.soup.get_text()
+    while True:
+        start_index = text.find('"url":"https://www.linkedin.com/feed/update/urn')
+        if start_index == -1:
+            break
+        result_links.append(text[start_index + 7 : start_index + 175])
+        text = text[start_index + 100 :]
+
+print("Found", len(result_links), "posts")
+print("Filtering posts...")
+filtered_posts = []
+file1 = open("file.txt", "a", encoding="utf-8")
+count = 0
+for link in result_links:
+    count += 1
+    print("Post -", count)
+    post_page = browser.get(link)
+    post_text = post_page.soup.get_text()
+    post_search = re.search(
+        'FEED_DETAIL,EMPTY,DEFAULT,false\)","relatedContent"', post_text
+    ).start()
+    posts = re.search('"text":".*?"', post_text[post_search:]).group()
+    posts = posts[8 : len(posts) - 1]
+    if check(posts):
+        filtered_posts.append(link)
+        file1.write(posts + "\n\n")
+
+print("Writing to file...")
+data = {"Links": filtered_posts}
+df1 = pd.DataFrame(data)
+df1.to_excel("output.xlsx")
+print("Successful")
